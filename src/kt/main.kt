0334a8e32ac0323506e8d62cec82ffb2d9a46fd6
Quang. Le Thai Phuc <quangltp@vng.com.vn>
2024-11-19 00:17:07 +0700

    WIP

--- src/kt/main.kt
+++ src/kt/main.kt
@@ -1,5 +1,7 @@
 package kt
 
+import kt.shortestSubarrayToBeRemoved.Solution1574
+
 /**
  * Created by Quang Le on 3/23/18.
  *
@@ -7,5 +9,6 @@ package kt
  * --Henry Ford--
  */
 fun main() {
-  println(SolutionMaxSubArray().maxSubArray(intArrayOf(-2,-1,-3,-4,-1,-2,-1,-5,-4)))
+//  println(SolutionMaxSubArray().maxSubArray(intArrayOf(-2,-1,-3,-4,-1,-2,-1,-5,-4)))
+  println(Solution1574().findLengthOfShortestSubarray(intArrayOf(1,2,3,10,4,2,3,5)))
 }
